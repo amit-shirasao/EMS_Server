ab240af7ee6a6fd8ac8d97078239bd0cafa44b63
Amit Shirasao <amit.shirasao@gmail.com>
2025-09-02 17:04:35 +0530

    NodeJS server with API calls.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Hello World.");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  switch (req.method) {
+    case "POST":
+      res.end("You want to create a new employee.");
+      break;
+
+    case "GET":
+      let id = req.url.split("/")[1];
+      if (id) {
+        res.end("You want to get a certain employee.");
+      } else {
+        res.end("You want to get all employees.");
+      }
+      break;
+
+    case "PUT":
+        res.end("You want to update a certain employee completely.");
+      break;
+
+    case "PATCH":
+        res.end("You want to update a certain employee partially.");
+      break;
+
+    case "DELETE":
+        res.end("You want to delete a certain employee.");
+      break;
+
+    default:
+      break;
+  }
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("Server is listening to http://localhost:3000.");
+});
